2b9f79219ce3658ab4c49947162b46242cc6c178
jaydiphorizoncore <jaydip.jadav@horizoncore.in>
2023-09-22 09:26:22 +0530

    space

--- app/src/main/java/com/example/mvvmdemowithroom/MainActivity.kt
+++ app/src/main/java/com/example/mvvmdemowithroom/MainActivity.kt
@@ -26,8 +26,6 @@ class MainActivity : AppCompatActivity(), NoteClickInterface, NoteClickDeleteInt
 
         noteRecyclerView.layoutManager = LinearLayoutManager(this)
         val noteAdapter = NoteAdapter(this, this)
-
-
         noteRecyclerView.adapter = noteAdapter
 
         // on below line we are
